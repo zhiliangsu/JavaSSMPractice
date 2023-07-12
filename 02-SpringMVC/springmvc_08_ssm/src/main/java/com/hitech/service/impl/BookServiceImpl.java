b1e422fd66676d0290309a2f00c17481ed0e6ccc
zhiliangsu <zhiliangsuzhiliang@gmail.com>
2023-07-12 17:51:22 +0800

    add module springmvc_08_ssm

---
+++ 02-SpringMVC/springmvc_08_ssm/src/main/java/com/hitech/service/impl/BookServiceImpl.java
@@ -0,0 +1,43 @@
+package com.hitech.service.impl;
+
+import com.hitech.dao.BookDao;
+import com.hitech.domain.Book;
+import com.hitech.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookServiceImpl implements BookService {
+    @Autowired
+    private BookDao bookDao;
+
+    @Override
+    public boolean save(Book book) {
+        bookDao.save(book);
+        return true;
+    }
+
+    @Override
+    public boolean delete(Integer id) {
+        bookDao.delete(id);
+        return true;
+    }
+
+    @Override
+    public boolean update(Book book) {
+        bookDao.update(book);
+        return true;
+    }
+
+    @Override
+    public Book getById(Integer id) {
+        return bookDao.getById(id);
+    }
+
+    @Override
+    public List<Book> getAll() {
+        return bookDao.getAll();
+    }
+}
