a26bf3257aea44676c3fb37de5ac6853fa706f97
zhiliangsu <zhiliangsuzhiliang@gmail.com>
2023-07-12 15:28:00 +0800

    add module springmvc_04_request_param

---
+++ 02-SpringMVC/springmvc_04_request_param/src/main/java/com/hitech/controller/UserController.java
@@ -0,0 +1,89 @@
+package com.hitech.controller;
+
+import com.hitech.domain.User;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+@Controller
+public class UserController {
+    @RequestMapping("/commonParam")
+    @ResponseBody
+    public String commonParam(String name, int age) {
+        System.out.println("普通参数传递 name ==> " + name);
+        System.out.println("普通参数传递 name ==> " + age);
+        return "{'module':'commonParam'}";
+    }
+
+    @RequestMapping("/commonParamDifferentName")
+    @ResponseBody
+    public String commonParamDifferentName(@RequestParam("name") String userName, int age) {
+        System.out.println("普通参数传递 name ==> " + userName);
+        System.out.println("普通参数传递 name ==> " + age);
+        return "{'module':'common param different name'}";
+    }
+
+    // POJO参数：请求参数与形参对象中的属性对应即可完成参数传递
+    @RequestMapping("/pojoParam")
+    @ResponseBody
+    public String pojoParam(User user) {
+        System.out.println("pojo参数传递 user ==> " + user);
+        return "{'module':'pojo param'}";
+    }
+
+    // 数组参数：同名请求参数可以直接映射到对应名称的形参数组对象中
+    @RequestMapping("/arrayParam")
+    @ResponseBody
+    public String arrayParam(String[] likes) {
+        System.out.println("数组参数传递 likes ==> " + Arrays.toString(likes));
+        return "{'module':'array param'}";
+    }
+
+    // 集合参数：同名请求参数可以使用@RequestParam注解映射到对应名称的集合对象中作为数据
+    @RequestMapping("/listParam")
+    @ResponseBody
+    public String listParam(@RequestParam List<String> likes) {
+        System.out.println("集合参数传递 likes ==> " + likes);
+        return "{'module':'list param'}";
+    }
+
+    // 使用@RequestBody注解将外部传递的json数组数据映射到形参的集合对象中作为数据
+    @RequestMapping("/listParamForJson")
+    @ResponseBody
+    public String listParamForJson(@RequestBody List<String> likes) {
+        System.out.println("list common(json)参数传递 list ==> " + likes);
+        return "{'module':'list common for json param'}";
+    }
+
+    @RequestMapping("/pojoParamForJson")
+    @ResponseBody
+    public String pojoParamForJson(@RequestBody User user) {
+        System.out.println("pojo(json)参数传递 user ==> " + user);
+        return "{'module':'pojo for json param'}";
+    }
+
+    @RequestMapping("/listPojoParamForJson")
+    @ResponseBody
+    public String listPojoParamForJson(@RequestBody List<User> list) {
+        System.out.println("list pojo(json)参数传递 list ==> " + list);
+        return "{'module':'list pojo for json param'}";
+    }
+
+    @RequestMapping("/dataParam")
+    @ResponseBody
+    public String dataParam(Date date,
+                            @DateTimeFormat(pattern = "yyyy-MM-dd") Date date1,
+                            @DateTimeFormat(pattern = "yyyy/MM/dd HH:mm:ss") Date date2) {
+        System.out.println("参数传递 date(默认yyyy/MM/dd) ==> " + date);
+        System.out.println("参数传递 date1(yyyy-MM-dd) ==> " + date1);
+        System.out.println("参数传递 date1(yyyy/MM/dd HH:mm:ss) ==> " + date2);
+        return "{'module':'data param'}";
+    }
+}
---
+++ 02-SpringMVC/springmvc_04_request_param/src/main/java/com/hitech/domain/Address.java
@@ -0,0 +1,13 @@
+package com.hitech.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address {
+    private String province;
+    private String city;
+}
