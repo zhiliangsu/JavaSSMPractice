1b29e5057c72573a9a219814fb3a5f37a6d25d75
zhiliangsu <zhiliangsuzhiliang@gmail.com>
2023-07-12 18:26:24 +0800

    add module springmvc_09_result

---
+++ 02-SpringMVC/springmvc_09_result/src/main/java/com/hitech/config/SpringMvcConfig.java
@@ -0,0 +1,11 @@
+package com.hitech.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan("com.hitech.controller")
+@EnableWebMvc
+public class SpringMvcConfig {
+}
