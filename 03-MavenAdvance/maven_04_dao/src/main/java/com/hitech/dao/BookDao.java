b38a95b19a5adc0685927302f7a84eff36451537
zhiliangsu <zhiliangsuzhiliang@gmail.com>
2023-07-13 18:21:43 +0800

    add module maven_01 --> maven_04

---
+++ 03-MavenAdvance/maven_04_dao/src/main/java/com/hitech/dao/BookDao.java
@@ -0,0 +1,27 @@
+package com.hitech.dao;
+
+import com.hitech.domain.Book;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
+
+public interface BookDao {
+    // @Insert("insert into tbl_book values(null, #{type}, #{name}, #{description})")
+    @Insert("insert into tbl_book(type, name, description) values(#{type}, #{name}, #{description})")
+    public int save(Book book);
+
+    @Delete("delete from tbl_book where id=#{id}")
+    public int delete(Integer id);
+
+    @Update("update tbl_book set type=#{type}, name=#{name}, description=#{description} where id=#{id}")
+    public int update(Book book);
+
+    @Select("select id, type, name, description from tbl_book where id=#{id}")
+    public Book getById(Integer id);
+
+    @Select("select id, type, name, description from tbl_book")
+    public List<Book> getAll();
+}
